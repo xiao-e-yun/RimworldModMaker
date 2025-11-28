aad79ea92a6a4efe9e709a7cd870f98783ee170b
xiao-e-yun <a0970323059@gmail.com>
2025-11-28 19:23:31 +0800

    feat: add StunnableComponent with damage type configuration

--- src/components/building/index.ts
+++ src/components/building/index.ts
@@ -1,4 +1,7 @@
 import { CompComponent } from ".."
+import { x, xls } from "@/xml"
+import { VanillaDamageDef } from "@/defs/vanilla"
+import type { DefId } from "@/defs"
 
 // Building Components
 export * from "./power"
@@ -8,15 +11,27 @@ export * from "./workbench"
 export * from "./door"
 export * from "./trap"
 export * from "./turret"
+export * from "./recipe"
 
 export const FlickableComponent = () => new CompComponent("CompProperties_Flickable", {
     isExtends: true,
 })
 
+export interface StunnableComponentProps {
+    /** Damage types that can stun this building. Defaults to [Stun, EMP] */
+    affectedDamageDefs?: DefId<"DamageDef">[]
+}
+
 /** Makes the building stunnable by EMP attacks */
-export const StunnableComponent = () => new CompComponent("CompProperties_Stunnable", {
-    isExtends: true,
-})
+export const StunnableComponent = (props: StunnableComponentProps = {}) => {
+    const damageDefs = props.affectedDamageDefs ?? [VanillaDamageDef.Stun, VanillaDamageDef.EMP]
+    return new CompComponent("CompProperties_Stunnable", {
+        isExtends: true,
+        props: [
+            x("affectedDamageDefs", xls(damageDefs)),
+        ],
+    })
+}
 
 /** Makes the building require a pawn to operate */
 export const MannableComponent = () => new CompComponent("CompProperties_Mannable", {
